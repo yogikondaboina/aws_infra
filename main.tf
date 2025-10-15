8e2db74464033e72b2a6d49b21d8b7c25b90b733
yogi <yogi>
2025-10-16 00:57:11 +0530

    push to github

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source      = "./terraform_module/s3"
+  source      = "./terraform_infra/terraform_module/s3"
   bucket_name = "my-terraform-s3-bucket"
   acl         = "private"
 }
