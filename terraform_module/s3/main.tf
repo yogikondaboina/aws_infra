c6e55ee060b63cd5e77361d5e675879f023494f0
yogi <yogi>
2025-10-16 19:52:24 +0530

    create s3

--- terraform_module/s3/main.tf
+++ terraform_module/s3/main.tf
@@ -1,3 +1,8 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.host_bucket_name
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
--- terraform_module/s3/variable.tf
+++ terraform_module/s3/variable.tf
@@ -1,5 +1,5 @@
 variable "host_bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default = "valhost-s3-bucketue"
+  default = "host-s1-bucket"
 }
