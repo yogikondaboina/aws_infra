7b4be1311e82518e37af41f80201bd281c0e427d
yogi <yogi>
2025-10-16 20:49:20 +0530

    create s1

--- terraform_module/s3/variable.tf
+++ terraform_module/s3/variable.tf
@@ -1,6 +1,7 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
+  default = "host_s1"
 }
 
 variable "acl" {
--- variable.tf
+++ variable.tf
@@ -7,4 +7,5 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
+  default = "host_s1"
 }
