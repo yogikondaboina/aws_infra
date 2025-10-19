cffe9e895ecffa37f76e1655f919d360e21747ac
yogi <yogi>
2025-10-19 14:32:37 +0530

    s3 creation

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket         = "store-state-d"
-    key            = "terraform/s3.tfstate"
+    key            = "s2.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
