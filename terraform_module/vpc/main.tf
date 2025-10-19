6aa341e1978e7df5f8eea7fca80cc362fc724751
yogi <yogi>
2025-10-19 20:27:02 +0530

    add private rout table

--- terraform_module/vpc/main.tf
+++ terraform_module/vpc/main.tf
@@ -76,9 +76,17 @@ resource "aws_route_table" "private" {
 
 # Associate Public Subnets with Public Route Table
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public_subnet)
-  subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = aws_route_table.public.id
+  count          = length(aws_subnet.private_subnet)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private.id
 }
+
+
+
+
+
+
+
+
 
 
