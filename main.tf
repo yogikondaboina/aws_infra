68ea043dcf99391743d7433a2c0e366db5c52412
yogi <yogi>
2025-10-19 20:42:17 +0530

    s3

--- main.tf
+++ main.tf
@@ -29,11 +29,11 @@ module "s3_bucket" {
 }
 
 
-module "my_vpc" {
-  source                = "./terraform_module/vpc"
-  vname                 = var.vname
-  vpc_cidr              = var.vpc_cidr
-  public_subnet_cidr    = var.public_subnet_cidr
-  private_subnet_cidr   = var.private_subnet_cidr
-  availability_zones    = var.availability_zones
-}
+# module "my_vpc" {
+#   source                = "./terraform_module/vpc"
+#   vname                 = var.vname
+#   vpc_cidr              = var.vpc_cidr
+#   public_subnet_cidr    = var.public_subnet_cidr
+#   private_subnet_cidr   = var.private_subnet_cidr
+#   availability_zones    = var.availability_zones
+# }
