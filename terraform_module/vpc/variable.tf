6c97b8d01deecf2f09d360414f92acf9e4bcb657
yogi <yogi>
2025-10-19 19:52:28 +0530

    add zones

--- terraform_module/vpc/variable.tf
+++ terraform_module/vpc/variable.tf
@@ -12,7 +12,7 @@ variable "vname" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default = "10.0.0.1/24"
+  default = "10.0.0.0/16"
 }
 
 # Public subnet CIDRs
--- variable.tf
+++ variable.tf
@@ -19,7 +19,7 @@ variable "vname" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default = "10.0.0.1/24"
+  default = "10.0.0.0/16"
 }
 
 # Public subnet CIDRs
