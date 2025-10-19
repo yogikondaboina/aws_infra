ac5ce1a108bffb74c375163f014da192dcb3aae1
yogi <yogi>
2025-10-19 21:03:40 +0530

    vpc

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_name" {
-  description = "The name of the created S3 bucket"
-  value       = module.s3_bucket.bucket_name
-}
-
-output "s3_bucket_arn" {
-  description = "The ARN of the created S3 bucket"
-  value       = module.s3_bucket.bucket_arn
-}
--- terraform_module/vpc/main.tf
+++ terraform_module/vpc/main.tf
@@ -76,8 +76,8 @@ resource "aws_route_table" "private" {
 
 # Associate Public Subnets with Public Route Table
 resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private_subnet)
-  subnet_id      = aws_subnet.private_subnet[count.index].id
+  count          = length(aws_subnet.private_private)
+  subnet_id      = aws_subnet.private_private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
