aca0afd16ede1852f9e5e02ea1e82660571c2d5a
yogi <yogi>
2025-10-19 19:40:44 +0530

    vpc creation

--- terraform_module/vpc/variable.tf
+++ terraform_module/vpc/variable.tf
@@ -12,6 +12,7 @@ variable "vname" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default = "10.0.0.1/24"
 }
 
 # Public subnet CIDRs
--- variable.tf
+++ variable.tf
@@ -19,6 +19,7 @@ variable "vname" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default = "10.0.0.1/24"
 }
 
 # Public subnet CIDRs
