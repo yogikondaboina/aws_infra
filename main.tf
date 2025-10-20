e70508eaa7670b8447ac9b867548a0d56628a8e4
yogi <yogi>
2025-10-20 14:28:56 +0530

    ec2

--- main.tf
+++ main.tf
@@ -17,10 +17,42 @@ terraform {
   }
 }
 
+variable "vpc_id" {}
+
 
 provider "aws" {
   region = var.aws_region
 }
+ 
+ ######### data sources ########
+data "aws_vpc" "data_vpc" {
+  id = 596122389379
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = data.aws_vpc.data_vpc
+    values = [data.aws_vpc.(10.0.1.0/24", "10.0.2.0/24)]
+  }
+
+  filter {
+    name   = "tag:string"
+    values = ["Public"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = data.aws_vpc.data_vpc
+    values = [data.aws_vpc.(10.0.3.0/24", "10.0.4.0/24)]
+  }
+
+  filter {
+    name   = "tag:string"
+    values = ["private"]
+  }
+}
+
 
 # module "s3_bucket" {
 #   source          = "./terraform_module/s3"
@@ -37,9 +69,11 @@ provider "aws" {
 #   availability_zones    = var.availability_zones
 # }
 
+# EC2 Instance
 module "ec2" {
   source     = "./terraform_module/ec2"
-  login_key  = var.login_key
   vname      = var.vname
-  vpc_id     = var.vpc_cidr
+  login_key  = var.login_key
+  #subnet_id = data.aws_subnet.public.id
+  #sg_id     = data.aws_security_group.ec2_sg.id
 }
