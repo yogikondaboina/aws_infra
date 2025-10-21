5876a67f10d8198ed01ce9457678f9f60cfd5389
yogi <yogi>
2025-10-21 22:00:02 +0530

    vpc destroy

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket         = "statesotre"
-    key            = "c2.tfstate" 
+    key            = "vpc.tfstate" 
     region         = "ap-south-1"
     encrypt        = true
   }
@@ -29,19 +29,19 @@ provider "aws" {
 #   acl             = "private"
 # }
 
-# module "my_vpc" {
-#   source                = "./terraform_module/vpc"
-#   vname                 = var.vname
-#   vpc_cidr              = var.vpc_cidr
-#   public_subnet_cidr    = var.public_subnet_cidr
-#   private_subnet_cidr   = var.private_subnet_cidr
-#   availability_zones    = var.availability_zones
-# }
-
-# EC2 Instance
-module "ec2" {
-  source        = "./terraform_module/ec2"
-  ami_id         = var.ami_id
-  vname         = var.vname
-  login_key      = var.login_key
+module "my_vpc" {
+  source                = "./terraform_module/vpc"
+  vname                 = var.vname
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_cidr    = var.public_subnet_cidr
+  private_subnet_cidr   = var.private_subnet_cidr
+  availability_zones    = var.availability_zones
 }
+
+# # EC2 Instance
+# module "ec2" {
+#   source        = "./terraform_module/ec2"
+#   ami_id         = var.ami_id
+#   vname         = var.vname
+#   login_key      = var.login_key
+# }
