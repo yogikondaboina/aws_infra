4fff2baf7b61388c55ee47292c0976ca6d5d8e56
yogi <yogi>
2025-10-21 14:51:21 +0530

    ec2 in public

--- terraform_module/ec2/main.tf
+++ terraform_module/ec2/main.tf
@@ -52,7 +52,7 @@ resource "local_file" "private_key" {
 resource "aws_instance" "ec2" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  subnet_id     = element(data.aws_subnets.public.ids, 0)
+  subnet_id     = data.aws_subnets.public.id
 
     tags = {
     Name = "${var.vname}-compute"
