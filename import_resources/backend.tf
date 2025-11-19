fba7fe6db6424d1b40e8bfb6262ef14cfd1f90ec
yogi <yogi>
2025-11-19 17:18:38 +0530

    s3 import

--- import_resources/backend.tf
+++ import_resources/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "statesotre"
-    key            = "s3import.tfstate" 
+    key            = "s3impor.tfstate" 
     region         = "ap-south-1"
     encrypt        = true
   }
