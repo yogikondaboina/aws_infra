fb66c16611196eddccd72fb2c26206c525487fb9
yogi <yogi>
2025-11-19 17:16:35 +0530

    s3 import

--- import_resources/import.tf
+++ import_resources/import.tf
@@ -1,4 +1,4 @@
 import {
   id = "trails3import"
-  to = aws_s3_bucket.s3_import
+  to = aws_s3_bucket.s3_impor
 }
--- import_resources/main.tf
+++ import_resources/main.tf
@@ -1,3 +1,3 @@
-resource "aws_s3_bucket" "s3_import" {
+resource "aws_s3_bucket" "s3_impor" {
   
 }
